29607b9b20e3d4f0b9dc807d0f37efdfaf1057d8
Joy Tan <weiling22@hotmail.com>
2021-07-21 00:36:17 +0800

    update DA with weird PCA

--- PythonTimeFile/daProject.py
+++ PythonTimeFile/daProject.py
@@ -21,7 +21,6 @@ df = pd.DataFrame(data = combined)
 # check if there is any null data
 np.where(df.applymap(lambda x: x == ''))
 
-df = df.rename(columns={"remaining_lease":"end_date"})
 df = df.sort_values(by=['street_name'])
 df["point_of_interest"] = 0
 df = df.reset_index(drop=True)
@@ -29,22 +28,22 @@ df = df.reset_index(drop=True)
 # 1 is true
 for i, row in df.iterrows():
     try:
-        datetime_object = datetime(2020, 2 ,2)
-        if "month" in row["end_date"]:
-            ttSplit = row["end_date"].split("years")
+       # datetime_object = datetime(2020, 2 ,2)
+        if "month" in row["remaining_lease"]:
+            ttSplit = row["remaining_lease"].split("years")
             year = int(ttSplit[0].strip()) * 12
             monthSplit = ttSplit[1].split("month")
             month = int(monthSplit[0].strip())
             totalMonth = year + month
         else:
-            monthSplit = row["end_date"].split("years")
+            monthSplit = row["remaining_lease"].split("years")
             totalMonth = int(monthSplit[0].strip()) * 12
-            row["month"] = row["month"].replace("-", " ")
-            datetime_object = datetime.strptime(row["month"], '%Y %m')
+        endDatetime = datetime.strptime(row["month"], '%Y-%m')
         #get the end date value
-        endDate = datetime_object + relativedelta(months=+totalMonth)
-        df.at[i, 'end_date'] = '{0}-{1:02}'.format(endDate.year ,endDate.month)
-
+        endDate = endDatetime + relativedelta(months=+totalMonth)
+        currentDate = datetime.now()
+        numberOfMonth = (endDate.year - currentDate.year) * 12 + (endDate.month - currentDate.month)
+        df.at[i, 'remaining_lease'] = numberOfMonth
         #print(df.loc[i,'street_name'])
         if i == 0 or (df.loc[i, 'street_name'] != df.loc[i-1, 'street_name']):
             #place api
@@ -68,50 +67,25 @@ for i, row in df.iterrows():
 df = df.drop(['block','street_name','searchval','building','postal.1','month'], axis=1)
 print(df)
 
-# there is no null data
-# df["travelTime"] = 0
-# googledf = df.copy()
-# googledf = googledf.drop_duplicates(subset='street_name',keep='first')
-# print(df)
-# index = googledf.index
-# for i, row in googledf.iterrows():
-#     try:
-#         destination = "822166"
-#         # defining the api-endpoint
-#         urlAPI = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&region=sg&mode=transit&origins='+str(row["latitude"])+','+ str(row["longtitude"]) +'&destinations='+destination+'&key=AIzaSyCJO7Y5uy8gF-y03PjMMBtPRQSjjpirb44 '
-#
-#         r = requests.post(url = urlAPI)
-#
-#         # extracting response text
-#         result = r.text
-#
-#         res = json.loads(result)
-#
-#         travelTime = res["rows"][0]["elements"][0]["duration"]["text"]
-#         print(travelTime)
-#
-#         if "hour" in travelTime:
-#             ttSplit = travelTime.split("hour")
-#             hour = int(ttSplit[0].strip()) * 60
-#             minSplit = ttSplit[1].split("min")
-#             min = int(minSplit[0].strip())
-#             totalTime = hour + min
-#         else:
-#             minSplit = travelTime.split("min")
-#             totalTime = int(minSplit[0].strip())
-#
-#         # googledf.iloc[i] = {"travelTime":totalTime}
-#         # print("total time = ", totalTime," Row ", googledf.iloc[i])
-#
-#         #print(res["rows"][0]["elements"][0]["duration"]["text"])
-#     except KeyError as e:
-#         print(e)
-#         #print(result)
-#
-# googledf = googledf.drop(['town', 'month','block','street_name','flat_model','remaining_lease','lease_commence_date','searchval','building','postal.1','postal'], axis=1)
-#
-# print(googledf)
+flatType = sorted(df["flat_type"].drop_duplicates())
+flatType
+i = 1
+for x in flatType:
+    df["flat_type"] = df["flat_type"].replace([x], i)
+    i = i + 1
 
+storeyRange = sorted(df["storey_range"].drop_duplicates())
+storeyRange
+i = 1
+for x in storeyRange:
+    df["storey_range"] = df["storey_range"].replace([x], i)
+    i = i + 1
 
-#print(df)
+flatModel = sorted(df["flat_model"].drop_duplicates())
+flatModel
+i = 1
+for x in flatModel:
+    df["flat_model"] = df["flat_model"].replace([x], i)
+    i = i + 1
 
+df
